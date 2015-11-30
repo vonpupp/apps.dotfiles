6614849edb1d66e0fb4e54cec98bc87e4985c0e3
vonpupp <vonpupp@gmail.com>
2015-11-30 00:42:41 -0200

    Reorganize script

--- home/bin/start-common.sh
+++ home/bin/start-common.sh
@@ -1,9 +1,18 @@
 #!/bin/zsh
 
 GEOM="300x200"
+
+# Productivity
 urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/task-dashboard' &
 #wmctrl -r TASK-DASH -b toggle,fullscreen &
+# mutt
 
-urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/start-ledger-es.sh' &
+# News and social
+urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/start-rainbowstream.sh' &
 urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/start-newsbeuter.sh' &
 urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/start-irssi.sh' &
+# cortex
+
+# Personal stuff
+urxvt -geometry $GEOM -e sh -c 'zsh -li ~/bin/start-ledger-es.sh' &
+# ncmcpp
