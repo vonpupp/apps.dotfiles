c834aab407d27d19d5c9f753e8627cad3e635cad
vonpupp <vonpupp@gmail.com>
2016-11-23 22:13:15 -0200

    Add parameter to keep intermediate files

--- home/bin/start-youtube-dl-folder.sh
+++ home/bin/start-youtube-dl-folder.sh
@@ -7,6 +7,7 @@ echo "PWD $PWD"
 echo "BASENAME $BASENAME"
 
 youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' \
+    -k \
     --batch-file="$PWD/urls.txt" --output "$PWD/%(title)s.%(ext)s" \
     -i --encoding utf-8 \
     --all-subs --write-srt --convert-subs srt $@
