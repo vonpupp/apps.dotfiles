632dd74f4768d06880021092a82b7adb334e8e69
Albert De La Fuente Vigliotti <albert@haevas.com>
2018-02-10 22:22:13 -0200

    Move to alock

--- home/bin/start-locker.sh
+++ home/bin/start-locker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+
+## i3lock
+# scrot /tmp/screenshot.png
+# convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+# i3lock -i /tmp/screenshotblur.png
+
+## alock
+alock -bg blank -auth pam -bg shade:shade=30,blur=70
