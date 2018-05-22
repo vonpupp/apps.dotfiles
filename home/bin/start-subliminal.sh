5cbb127ca9f999178184a905a06082b46e83e049
vonpupp <albert@haevas.com>
2018-05-21 22:42:43 -0300

    Add subliminal wrapper

--- home/bin/start-subliminal.sh
+++ home/bin/start-subliminal.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+fullfilename=$(basename -- "$@")
+fullpath=$(pwd)
+extension="${fullfilename##*.}"
+filename="${fullfilename%.*}"
+
+fqfilename="$fullpath/$fullfilename"
+
+# Fetch subs
 cd /home/av/bin/pipenv3
-
-pipenv run subliminal download -l en "$@"
-pipenv run subliminal download -l es "$@"
-pipenv run subliminal download -l pt-BR "$@"
-pipenv run subliminal download -l fr "$@"
-pipenv run subliminal download -l it "$@"
-
+pipenv run subliminal download -l en "$fqfilename"
+pipenv run subliminal download -l es "$fqfilename"
+pipenv run subliminal download -l pt-BR "$fqfilename"
+pipenv run subliminal download -l fr "$fqfilename"
+pipenv run subliminal download -l it "$fqfilename"
 cd -
+
+# Get rid of BOM's if already UTF-8
+dos2unix *.srt
+
+# Convert to UTF-8
+for file in *.srt
+do
+    iconv -c -f ISO-8859-1 -t UTF-8 -o "$file.new" "$file" &&
+    mv -f "$file.new" "$file"
+done
