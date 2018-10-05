0b02f6da9eef65af75626a2dbddb6260d3f7b95f
vonpupp <albert@haevas.com>
2018-10-05 10:42:03 -0300

    Add available updates autorun script

---
+++ home/bin/archlinux-available-updates-autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Source:
+# https://shapeshed.com/managing-packages-on-arch-linux/
+# https://bbs.archlinux.org/viewtopic.php?id=184002
+
+PAC_UPDATES=$(checkupdates | wc -l)
+KERNEL_UPDATES=$(checkupdates | pcregrep '^linux(-[^-\s]+)*(?<!-api)-headers$')
+#AUR_UPDATES=$(cower -u | wc -l)
+
+echo "Pacman available updates: $PAC_UPDATES"
+#echo "AUR available updates: $AUR_UPDATES"
+
+#if [ "$PAC_UPDATES" -lt 20 ] && [ "$KERNEL_UPDATES" -eq 0 ]; then
+#    exit 0
+#if [ "$PAC_UPDATES" -ge 20 ] || [ "$KERNEL_UPDATES" -ge 1 ]; then
+if [ "$PAC_UPDATES" -ge 20 ]; then
+    echo "break"
+    #notify-send "Pending updates:" "$(checkupdates)";
+    #echo "PACKAGES: [ $PAC_UPDATES ]"
+fi
