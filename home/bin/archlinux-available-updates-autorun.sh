cb2d0d2cba15db568b5f0e75a78b851fe0fbdad3
vonpupp <albert@haevas.com>
2020-01-14 23:22:16 -0300

    Add random for fetching updates

--- home/bin/archlinux-available-updates-autorun.sh
+++ home/bin/archlinux-available-updates-autorun.sh
@@ -4,22 +4,26 @@
 # https://shapeshed.com/managing-packages-on-arch-linux/
 # https://bbs.archlinux.org/viewtopic.php?id=184002
 
-AVAILABLE_UPDATES=$(checkupdates)
-PAC_UPDATES=$(echo "$AVAILABLE_UPDATES" | wc -l)
-KERNEL_UPDATES=$(echo "$AVAILABLE_UPDATES" | pcregrep '^linux ' | wc -l)
-#AUR_UPDATES=$(cower -u | wc -l)
+RND=$(( ( RANDOM % 10 )  + 1 ))
+echo "RND=$RND"
+if [ "$RND" -le 3 ]; then
+    AVAILABLE_UPDATES=$(checkupdates)
+    PAC_UPDATES=$(echo "$AVAILABLE_UPDATES" | wc -l)
+    KERNEL_UPDATES=$(echo "$AVAILABLE_UPDATES" | pcregrep '^linux ' | wc -l)
+    #AUR_UPDATES=$(cower -u | wc -l)
 
-# Dirty solution for trailing CRLF when no updates, not such a big deal
-if [ "$PAC_UPDATES" -ge 2 ]; then
-    echo "PACMAN available updates: $PAC_UPDATES"
-fi
-if [ "$KERNEL_UPDATES" -ge 1 ]; then
-    echo "KERNEL available updates: $KERNEL_UPDATES"
-fi
-#if [ "$AUR_UPDATES" -ge 1 ]; then
-#    echo "   AUR available updates: $AUR_UPDATES"
-#fi
+    # Dirty solution for trailing CRLF when no updates, not such a big deal
+    if [ "$PAC_UPDATES" -ge 2 ]; then
+        echo "PACMAN available updates: $PAC_UPDATES"
+    fi
+    if [ "$KERNEL_UPDATES" -ge 1 ]; then
+        echo "KERNEL available updates: $KERNEL_UPDATES"
+    fi
+    #if [ "$AUR_UPDATES" -ge 1 ]; then
+    #    echo "   AUR available updates: $AUR_UPDATES"
+    #fi
 
-if [ "$PAC_UPDATES" -ge 20 ]; then
-    notify-send "Pending updates:" "$(echo "$AVAILABLE_UPDATES")";
+    if [ "$PAC_UPDATES" -ge 20 ]; then
+        notify-send "Pending updates:" "$(echo "$AVAILABLE_UPDATES")";
+    fi
 fi
